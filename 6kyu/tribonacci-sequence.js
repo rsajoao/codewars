c472a56d529f0b99308382c781fb9d6e929afb89
Joao Vitor <joaovitors@outlook.com>
2023-10-21 16:33:24 -0300

    tribonacci sequence (adicionando alguns consoles)

--- 6kyu/tribonacci-sequence.js
+++ 6kyu/tribonacci-sequence.js
@@ -3,8 +3,10 @@ function tribonacci(signature, n) {
     const threeLast = signature.slice(signature.length - 3);
     signature.push(threeLast.reduce((total, value) => total + value, 0))
   }
-
   return signature.slice(0, n);
 }
 
 console.log(tribonacci([1, 1, 1], 10));
+console.log(tribonacci([1, 1, 1], 2));
+console.log(tribonacci([0, 1, 1], 10));
+console.log(tribonacci([1, 1, 1], 0));
